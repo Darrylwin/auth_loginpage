423e0860e3daeead08a803914936bffaae7d1069
Darrylwin <logossoudarryl20@gmail.com>
2024-09-27 01:42:06 +0000

    component auth_button

---
+++ lib/componenets/auth_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AuthButton extends StatelessWidget {
+  AuthButton({
+    super.key,
+    required this.path,
+    required this.auth,
+  });
+  final String path;
+  final String auth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          border: Border.all(
+            color: Colors.grey,
+            style: BorderStyle.solid,
+          ),
+        ),
+        height: 43,
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(path),
+            const SizedBox(width: 8),
+            Text(
+              auth,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
