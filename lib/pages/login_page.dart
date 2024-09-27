37c5d5240177842f278878386f4b06dbe5c2fd3c
Darrylwin <logossoudarryl20@gmail.com>
2024-09-27 19:05:59 +0000

    import package google_sign_in

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/componenets/auth_button.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
